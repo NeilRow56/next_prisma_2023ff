65bb78940406d89c9795a8f9eb01f99836313556
Neil <neilrowland56@yahoo.com>
2023-03-06 11:03:28 +0000

    Added final PostSearch.jsx file as a component to app/search/page.jsx

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import PostSearch from "@/components/PostSearch";
 import Spinner from "@/components/Spinner";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
@@ -45,28 +46,8 @@ const SearchPage = () => {
         Showing results for:{" "}
         <span className="font-semibold">{searchQuery}</span>
       </span>
-      <div>
-        {data.posts.map((post) => (
-          <div
-            key={post.id}
-            className="my-3 flex w-3/4 gap-4 rounded-xl border-[1px] border-zinc-600 p-3"
-          >
-            <div>
-              <Image
-                src={post.user.image || ""}
-                alt="avatar"
-                height={52}
-                width={52}
-                className="rounded-full"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <span className="text-xl font-semibold">{post.user.name}</span>
-              <span className="text-lg font-light">{post.title}</span>
-            </div>
-          </div>
-        ))}
-      </div>
+
+      <PostSearch posts={data.posts} />
     </>
   );
 };
--- components/PostSearch.jsx
+++ components/PostSearch.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const PostSearch = ({ posts }) => {
   return (
     <>
@@ -6,6 +8,15 @@ const PostSearch = ({ posts }) => {
           key={post.id}
           className="my-3 flex w-3/4 gap-4 rounded-xl border-[1px] border-zinc-600 p-3"
         >
+          <div>
+            <Image
+              src={post.user.image || ""}
+              alt="avatar"
+              height={52}
+              width={52}
+              className="rounded-full"
+            />
+          </div>
           <div className="flex flex-col gap-2">
             <span className="text-xl font-semibold">{post.user.name}</span>
             <span className="text-lg font-light">{post.title}</span>
